8650d30f86f22fbf2777e498fe5e84286aaeb55f
Amin Amos <aminamosahs@gmail.com>
2019-06-13 14:44:24 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
     
     loop do
-      print phrase
+      puts phrase
       counter += 1
     if counter >= number_of_times
       break
